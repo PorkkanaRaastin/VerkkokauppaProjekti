0ef60dec2cb00eab34e2505d78acb638cc6f7bda
SAVIKIVI <savikivi143@gmail.com>
2026-04-17 10:28:57 +0300

    commit

---
+++ store.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Store</title>
+        <link rel="stylesheet" href="store.css">
+        <link rel="stylesheet" href="grid.css">
+    </head>
+    <body>
+        <div class="gridLayout">
+            
+        </div>
+    </body>
+</html>
