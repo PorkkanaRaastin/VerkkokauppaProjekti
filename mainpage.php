5b2ac92ee7f6de76f23efba03a526b26a977b714
PorkkanaRaastin <rasmusthegamer1@gmail.com>
2026-04-17 10:55:35 +0300

    etusivumuokkauksia

--- mainpage.php
+++ mainpage.php
@@ -1,12 +1,33 @@
 <!DOCTYPE html>
-<html lang="">
+<html lang="fi">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Online Store</title>
-    <link rel="stylesheet" href="homepage.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tuottajatori</title>
+  <link rel="stylesheet" href="homepage.css">
 </head>
 <body>
+    
+  <div class="hero-section"></div>
+
+  <header>
+    <h1>Tuoretta lähiruokaa suoraan kotiovellesi!</h1>
+    <p>Tuottajatorilta - Kätevä palvelu vastuullisten lähiruokien hankintaan.</p>
+    <button>Selaa tuotteita</button>
+  </header>
+
+  <section id="how-it-works">
+    <h2>Kuinka Se Toimii</h2>
+    <ol>
+      <li><strong>Rekisteröityminen:</strong> Luo käyttäjätunnus ja salasana.</li>
+      <li><strong>Selaa tuotteita:</strong> Löydä lähituottajien tuotteita ja tarjouksia.</li>
+      <li><strong>Lisää ostoskoriin:</strong> Valitse haluamasi tuotteet.</li>
+      <li><strong>Tarkista ostokset:</strong> Varmista ostoskorin sisältö.</li>
+      <li><strong>Tilauksen vahvistus:</strong> Saat sähköpostiisi vahvistuksen.</li>
+      <li><strong>Toimitus kotiovelle:</strong> Tuotteet toimitetaan sovittuna ajankohtana.</li>
+      <li><strong>Nauti lähiruoasta:</strong> Nauti tuoreista ja laadukkaista tuotteista.</li>
+    </ol>
+  </section>
 
 </body>
 </html>
