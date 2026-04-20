498e9434fdadd6441e1aa0fa92ec98a2a177af6c
SAVIKIVI <savikivi143@gmail.com>
2026-04-20 10:34:23 +0300

    added store page

--- store.php
+++ store.php
@@ -4,27 +4,51 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Store</title>
+        <meta charset="UTF-8">
+        <title>Verkkokauppa</title>
         <link rel="stylesheet" href="store.css">
-        <link rel="stylesheet" href="grid.css">
     </head>
     <body>
-        <div class="gridLayout">
-            <div id="productDisplay">
-                <?php
-                    $query="SELECT Product.name, Category.name AS 'category', Product.description, Product.prize FROM Product INNER JOIN Category ON Product.categoryId = Category.categoryId";
-                    $result=$link->query($query);
-                    while($data=$result->fetch_assoc()){
-                        $name=$data["name"];
-                        $category=$data["category"];
-                        $description=$data["description"];
-                        $prize=$data["prize"];
-                        echo"<div class='productContainer'>";
-
-                        echo"</div>";
-                    };
-                ?>
+        <main>
+            <header class="header">
+                <div>LOGO</div>
+                <div>🛒 Etusivu</div>
+            </header>
+            <h1>Verkkokauppa</h1>
+            <input type="text" placeholder="Etsi tuotteita..." class="search">
+            <div class="filters">
+                <label><input type="checkbox" checked> Kaikki</label>
+                <label><input type="checkbox"> Kala</label>
+                <label><input type="checkbox"> Liha</label>
+                <label><input type="checkbox"> Viljatuotteet</label>
+                <label><input type="checkbox"> Marjat</label>
+                <label><input type="checkbox"> Juustot</label>
+                <label><input type="checkbox"> Muut tuotteet</label>
             </div>
-        </div>
+            <div class="products">
+                <?php
+                    $query="SELECT Products.name, Products.categoryId, Products.description, Products.prize FROM Products";
+                    $result=$link->query($query);
+                    if($result->num_rows==0){
+                        echo"No results.";
+                    }else{while($data=$result->fetch_assoc()){
+                        $name=$data["name"];
+                        $targetId=$data["categoryId"];
+                        $category=$link->query("SELECT name FROM Categories WHERE categoryId = '$targetId'")->fetch_assoc()["name"];
+                        $description=$data["description"];
+                        $prize=round(floatval($data["prize"]),2)."€";
+                        echo"<div class='card'>
+                        <h3>$name</h3>
+                        <p class='category'>$category</p>
+                        <span>$description</span>
+                        <div class='bottom'>
+                        <span>$prize</span>
+                        <button>Add to cart</button>
+                        </div>
+                        </div>";
+                    };};
+                ?>  
+            </div>
+        </main>
     </body>
 </html>
