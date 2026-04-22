3cf1f961b7f7d23cbd852fd6ba4952736008e757
PorkkanaRaastin <rasmusthegamer1@gmail.com>
2026-04-22 08:41:16 +0300

    jotain

--- mainpage.php
+++ mainpage.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Tuottajatori</title>
   <link rel="stylesheet" href="homepage.css">
+  <link rel="icon" href="images/FAVICON.png" type="image/png">
 </head>
 <body>
     <div class="bgimage">
