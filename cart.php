0266e3864f99ce77b104bf90294cc51720c3afb2
SAVIKIVI <savikivi143@gmail.com>
2026-04-23 10:36:09 +0300

    poista toi

--- cart.php
+++ cart.php
@@ -28,39 +28,44 @@
     <div class="container">
         <section class="cart-items">
         <?php
+            $totalPrize=0.00;
             $userId=$_SESSION["userId"];
-            $query="SELECT Cart.cartId FROM Cart WHERE Cart.userId LIKE '$userId'";
+            $query="SELECT Orders.orderId FROM Orders WHERE Orders.userId LIKE '$userId' AND Orders.status LIKE 'FILL'";
             $result=$link->query($query);
-            if($result->num_rows!=0){
-                $cartId=$result->fetch_assoc()["cartId"];
+            if($result->num_rows==0){
+                $time=date("Y")."-".date("m")."-".date("d");
+                $query="INSERT INTO Orders (time, userId, status) VALUES ('$time', '$userId', 'FILL')";
+                $result=$link->query($query);
             };
-            $orderId="SELECT Cart.orderId FROM Cart WHERE Cart.cartId LIKE '$cartId'";
+            $query="SELECT Orders.orderId FROM Orders WHERE Orders.userId LIKE '$userId' AND Orders.status LIKE 'FILL'";
             $orderId=$link->query($query)->fetch_assoc()["orderId"];
-            $query="SELECT CartItem.productId, CartItem.amount FROM CartItem WHERE CartItem.cartId LIKE '$cartId'";
+            $query="SELECT OrderItem.productId, OrderItem.amount FROM OrderItem WHERE OrderItem.orderId LIKE '$orderId'";
             $result=$link->query($query);
-            $totalPrize=0;
-            while($data=$result->fetch_assoc()){
+            if($result->num_rows==0){
+                echo"Ostoskorisi on tyhjä";
+            }else{while($data=$result->fetch_assoc()){
                 $productId=$data["productId"];
-                $itemAmount=$data["amount"];
-                $query="SELECT Products.name, Products.prize, Products.categoryId FROM Products WHERE Products.productId LIKE '$productId'";
-                $tempResult=$link->query($query)->fetch_assoc();
-                $prize=$tempResult["prize"];
-                $name=$tempResult["name"];
-                $categoryId=$temp["categoryId"];
+                $amount=$data["amount"];
+                $query="SELECT Products.name, Products.categoryId, Products.prize, Products.description FROM Products WHERE Products.productId LIKE '$productId'";
+                $details=$link->query($query)->fetch_assoc();
+                $categoryId=$details["categoryId"];
+                $prize=round($details["prize"],2);
+                $productName=$details["name"];
+                $description=$details["description"];
                 $query="SELECT Categories.name FROM Categories WHERE Categories.categoryId LIKE '$categoryId'";
-                $categoryName=$link->query($query)->fetch_assoc()["name"];
+                $category=$link->query($query)->fetch_assoc()["name"];
                 echo"<div class='item'>
-                <h3>$name</h3>
-                <p>$categoryName</p>
+                <h3>$productName</h3>
+                <p>$category</p>
                 <div class='row'>
-                <input type='number' value='$itemAmount' min='1'>
-                <span>$prize<span>
-                </div></div>";
-                $totalPrize+=$prize;
-            };
+                <input type='number' value='$amount' min='1'>
+                <span>$prize</span>
+                </div>
+                </div>";
+                $totalPrize+=($prize*$amount);
+            };};
+            echo"<div class='total'>$totalPrize</div>";
         ?>
-
-        <div class="total"><?phpecho$totalPrize;?></div>
         </section>
 
         <section class="order-form">
@@ -73,15 +78,8 @@
             </form>
             <?php
                 if(isset($_POST["sendOrder"])){
-                    $userId=$_SESSION["userId"];
-                    $query="SELECT Cart.cartId, Cart.orderId FROM Cart WHERE Cart.userId LIKE $userId";
-                    $result=$link->query($query);
-                    if($result->num_rows==0){
-                        echo"Ostoskori on tyhjä";
-                    }else{
-                        // update the cart-item
-                    };
-                };
+                    // set order status to SENT
+                }
             ?>
         </section>
     </div>
