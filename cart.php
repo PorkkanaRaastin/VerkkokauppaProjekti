cc1e6ed747edc574187eb8b59e993a98c3337044
PorkkanaRaastin <rasmusthegamer1@gmail.com>
2026-04-24 09:16:30 +0300

    ostoskori

--- cart.php
+++ cart.php
@@ -2,7 +2,7 @@
     include_once("link.php");
     session_start();
     if(!isset($_SESSION["userId"])){
-        //header("Location: login.php");
+        header("Location: login.php");
     };
     $userId=$_SESSION["userId"];
     if(isset($_POST["sendOrder"])){
@@ -53,13 +53,16 @@
         <link rel="stylesheet" href="styles/cart.css">
     </head>
     <body>
-        <header>
-            <div class="logo">LOGO</div>
             <nav>
-                <a href="mainpage.php">Etusivu</a>
-                <a href="store.php">Kauppa</a>
+                <a href="mainpage.php">
+                    <img src="images/ALTERNATE LOGO_lapinakuva.png" alt="logo" class="nav-logo">
+                </a>
+                <a href="store.php">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-shop" viewBox="0 0 16 16">
+                        <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.37 2.37 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0M1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5M4 15h3v-5H4zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1zm3 0h-2v3h2z"/>
+                    </svg>
+                </a>
             </nav>
-        </header>
         <main>
             <h1>Ostoskori</h1>
             <div class="container">
@@ -95,11 +98,11 @@
                             <h3>$productName</h3>
                             <p>$category</p>
                             <div class='row'>
-                            <form action='' method='post'>
+                            <form class='orders' action='' method='post'>
                             <input name='newAmount' type='number' value='$amount' min='1'>
                             <input type='hidden' name='productId' value='$productId'>
-                            <button type='submit' name='changeAmount'>OK</button>
-                            <button type='submit' name='deleteOrderItem'>Poista ostoskorista</button>
+                            <button style='color: lightgreen;' type='submit' name='changeAmount'>OK</button>
+                            <button style='color: coral;' type='submit' name='deleteOrderItem'>Poista ostoskorista</button>
                             </form>
                             <span>$prize €</span>
                             </div>
@@ -110,12 +113,12 @@
                     ?>
                 </section>
                 <section class="order-form">
-                    <form action="" method="post">
+                    <form class="order-form-info" action="" method="post">
                         <h2>Tilaustiedot</h2>
                         <input name="name" type="text" placeholder="Nimi">
                         <input name="email" type="email" placeholder="Sähköpostiosoite">
                         <input name="phone" type="tel" placeholder="Puhelinnumero">
-                        <button type="submit" name="sendOrder">Tee tilaus</button>
+                        <button type="submit" name="sendOrder">Tee tilaus >>></button>
                     </form>
                 </section>
             </div>
