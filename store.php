3c6e5e0f915b4b3720d79c321a576a1455e85c3c
PorkkanaRaastin <rasmusthegamer1@gmail.com>
2026-04-28 10:08:48 +0300

    svg -> google icons

--- store.php
+++ store.php
@@ -59,10 +59,8 @@
                         };
                         // $OrderItemCount = tavaran määrä
                     ?>
-                    <a href="cart.php">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
-                            <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/>
-                        </svg>
+                    <a class="svg"href="cart.php">
+                        <img src="images\shopping_cart_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg" alt="svg"">
                     </a>
                 </div>
             </header>
